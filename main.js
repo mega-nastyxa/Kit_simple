6eabf5b3b89ffce0fc3d232bf61338b8ad296af9
Anastasia Kravchenko <142917747+mega-nastyxa@users.noreply.github.com>
2024-11-23 16:31:40 +0300

    updated dialog and media to dialog

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('open');
 })
 
-// document.querySelector('.burger').addEventListener('click', function() {
-//     this.classList.toggle('active');
-//     document.querySelector('.header__contacts').classList.toggle('open');
-// })
+const updateButton = document.getElementById("updateDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("favDialog");
+dialog.returnValue = "favAnimal";
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+});
